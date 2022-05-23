3f20f1e73659f7a022de7aac1a3dbba2457b2e95
Kristiiina Pokk <kristiiinapokk@KristiisMacBook.lan>
2022-05-23 16:44:14 +0300

    Adding descriptice analysis prep

--- Network analysis/FileHelper.py
+++ Network analysis/FileHelper.py
@@ -10,3 +10,28 @@ def drop_sentiment(df):
 def core_to_file(G, target):
     df = nx.to_pandas_edgelist(G)
     df.to_csv('./Data/K_cores/'+target+'.csv', index=False)
+
+
+def to_df(source):
+    df = pd.read_csv(source)
+    df = df.astype({'source': 'int64', 'target': 'int64'})
+    return df
+
+
+def switch_direction(df):
+    return df.rename(columns={"source": "target", "target": "source"})
+
+
+def collaps_to_wo_sentiment(df):
+    df_collapsed = df.drop(columns=['sentiment'])
+    df_collapsed = df_collapsed.groupby(['source', 'target']).agg({
+        'weight': 'sum'}).reset_index()
+    return df_collapsed
+
+
+def graph_to_csv(df, name):
+    df.to_csv('./Data/'+name+'.csv', index=False)
+
+
+def collapsed_to_csv(df, name):
+    df.to_csv('./Data/Collapsed/'+name+'.csv', index=False)
